62da0d53601c0f64c1e1cd45ac6e74328b904d6c
coderwithme9876-debug <coderwithme9876@gmail.com>
2026-07-20 13:13:15 +0530

    updated variable.cpp

--- variable.cpp
+++ variable.cpp
@@ -11,13 +11,24 @@
 
 // Accessing and Updating
 
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int num = 3;
+//   cout << num << endl;
+//   num = 7;
+//   cout << num;
+//   return 0;
+// }
+
+// Assigning num1's value to num2
 #include <iostream>
 using namespace std;
 int main()
 {
-  int num = 3;
-  cout << num << endl;
-  num = 7;
-  cout << num;
+  int num1 = 20, num2;
+  num2 = num1;
+  cout << num1 << " " << num2;
   return 0;
 }
