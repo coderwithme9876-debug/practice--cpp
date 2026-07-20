cb2ff705c4d163e1d9b600d1172c6032670b298c
coderwithme9876-debug <coderwithme9876@gmail.com>
2026-07-20 13:04:59 +0530

    updated variable.cpp

--- variable.cpp
+++ variable.cpp
@@ -1,10 +1,23 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int num = 4;
+//   cout << num << endl;
+//   int num1 = 5;
+//   cout << num1;
+//   return 0;
+// }
+
+// Accessing and Updating
+
 #include <iostream>
 using namespace std;
 int main()
 {
-  int num = 4;
+  int num = 3;
   cout << num << endl;
-  int num1 = 5;
-  cout << num1;
+  num = 7;
+  cout << num;
   return 0;
 }
